d2928172ef89a1f7557d1f8cd05dbdaa8de8dc3e
vivek sharma <vsharma1985@gmail.com>
2019-12-04 10:03:28 +0530

    first Commit

---
+++ src/main/java/com/zuulAPIgateway/springbootzuulapigatewayproxy/SpringBootZuulApigatewayproxyApplication.java
@@ -0,0 +1,34 @@
+package com.zuulAPIgateway.springbootzuulapigatewayproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class SpringBootZuulApigatewayproxyApplication {
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootZuulApigatewayproxyApplication.class, args);
+    }
+ 
+    @Bean
+    public PreFilter preFilter() {
+        return new PreFilter();
+    }
+    @Bean
+    public PostFilter postFilter() {
+        return new PostFilter();
+    }
+    @Bean
+    public ErrorFilter errorFilter() {
+        return new ErrorFilter();
+    }
+    @Bean
+    public RouteFilter routeFilter() {
+        return new RouteFilter();
+    }
+}
---
+++ src/test/java/com/zuulAPIgateway/springbootzuulapigatewayproxy/SpringBootZuulApigatewayproxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuulAPIgateway.springbootzuulapigatewayproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootZuulApigatewayproxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
